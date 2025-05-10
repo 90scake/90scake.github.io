9056186d6dae6543bc570ba5b87cc57d64662f87
niamul.hasan <niamul.hasan@brotecs.com>
2025-05-10 18:20:26 +0600

    added and index to handel error

--- production_app_making/src/App.jsx
+++ production_app_making/src/App.jsx
@@ -1,10 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import PoundCakes from "./pages/PoundCakes";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirectPath = params.get("redirect");
+
+    if (redirectPath === "/pound-cakes") {
+      navigate(redirectPath);
+      console.log("redirectPath", redirectPath);
+    }
+  }, [navigate]);
   return (
     <div className="flex flex-col min-h-screen bg-pink-50">
       <Navbar />
